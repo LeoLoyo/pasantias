de0329dd9bafb07c6e36d3c20bf8cfee740e097d
Leonardo Loyo <cyberedward@gmail.com>
2015-06-05 16:56:22 -0430

    actualizacion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,46 +4,53 @@
 		
 		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 
-		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				color: #B0BEC5;
-				display: table;
-				font-weight: 100;
-				font-family: 'Lato';
-			}
+		{!! Html::style('bower_components/bootstrap/dist/css/bootstrap.min.css') !!}
+		{!! Html::style('bower_components/bootstrap-material-design/dist/css/material.min.css') !!}
+		{!! Html::style('bower_components/bootstrap-material-design/dist/css/ripples.min.css') !!}
+		{!! Html::style('bower_components/bootstrap-material-design/dist/css/roboto.min.css') !!}
+		{!! Html::style('bower_components/bootstrap-material-design/dist/css/material-fullpalette.min.css') !!}
 
-			.container {
-				text-align: center;
-				display: table-cell;
-				vertical-align: middle;
-			}
-
-			.content {
-				text-align: center;
-				display: inline-block;
-			}
-
-			.title {
-				font-size: 96px;
-				margin-bottom: 40px;
-			}
-
-			.quote {
-				font-size: 24px;
-			}
-		</style>
 	</head>
 	<body>
-		<div class="container">
-			<div class="content">
-				<div class="title">Sistema De Gestion de Fallas | Transbarca</div>
-				<!--<div class="quote">{{ Inspiring::quote() }}</div>
-				-->
-			</div>
-		</div>
+	<div class="container">
+						<form class="form-horizontal">
+					<fieldset>
+
+					<!-- Form Name -->
+					<legend>Form Name</legend>
+
+					<!-- Text input-->
+					<div class="control-group">
+					  <label class="control-label" for="Usuario">Usuario</label>
+					  <div class="controls">
+					    <input id="Usuario" name="Usuario" type="text" placeholder="" class="input-xlarge">
+					    
+					  </div>
+					</div>
+
+					<!-- Password input-->
+					<div class="control-group">
+					  <label class="control-label" for="Contraseña">Contraseña:</label>
+					  <div class="controls">
+					    <input id="Contraseña" name="Contraseña" type="password" placeholder="" class="input-xlarge">
+					    
+					  </div>
+					</div>
+
+					<!-- Button (Double) -->
+					<div class="control-group">
+					  <label class="control-label" for="button1id"></label>
+					  <div class="controls">
+					    <button id="button1id" name="button1id" class="btn btn-success">Iniciar Sesion</button>
+					    <button id="button2id" name="button2id" class="btn btn-danger">Cancelar</button>
+					  </div>
+					</div>
+
+					</fieldset>
+					</form>
+					</div>
+
+
+
 	</body>
 </html>
